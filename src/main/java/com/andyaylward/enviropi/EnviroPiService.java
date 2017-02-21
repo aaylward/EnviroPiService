11ea7b1601f34e13cdc1abe8ef426c900495616f
Andy Aylward <aaylward@gmail.com>
2017-02-21 08:51:59 -0500

    this doesn't throw

--- src/main/java/com/andyaylward/enviropi/EnviroPiService.java
+++ src/main/java/com/andyaylward/enviropi/EnviroPiService.java
@@ -4,10 +4,8 @@ import com.andyaylward.enviropi.config.EnviroModule;
 import com.muchq.lunarcat.Service;
 import com.muchq.lunarcat.config.Configuration;
 
-import java.io.IOException;
-
 public class EnviroPiService {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     Configuration configuration = Configuration.newBuilder()
         .withBasePackage(EnviroPiService.class.getPackage())
         .withModules(new EnviroModule())
