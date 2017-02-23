ce45e22967bac5ace07119e6c3a8bc10dc1e4c06
Andy Aylward <aaylward@gmail.com>
2017-02-22 22:45:43 -0500

    return json

--- src/main/java/com/andyaylward/enviropi/filters/HeaderAuthFilter.java
+++ src/main/java/com/andyaylward/enviropi/filters/HeaderAuthFilter.java
@@ -4,16 +4,19 @@ import com.andyaylward.enviropi.auth.ApiKey;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
+import javax.ws.rs.Produces;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.security.MessageDigest;
 
 @Provider
+@Produces(MediaType.APPLICATION_JSON)
 public class HeaderAuthFilter implements ContainerRequestFilter {
   private static final String API_KEY_HEADER_NAME = "enviro-key";
   private static final Response FOUR_OH_ONE = Response
